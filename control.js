62d0677343a068823f9654b013826e759e0a366c
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2019-02-13 12:13:14 +0530

    Added bricks to the game.

--- control.js
+++ control.js
@@ -1,6 +1,7 @@
-let paddle = new Paddle(180, 30, 740, 0, 960);
-let ball = new Ball(80, 0, 440, 960, 780);
-let game = new Game(ball, paddle);
+let paddle = new Paddle(180, 30, 740, 600, 964);
+let ball = new Ball(30, 300, 440, 964, 780);
+let bricks = new Bricks(100, 30, 964);
+let game = new Game(ball, paddle, bricks);
 
 const createPaddle = function(paddle) {
   let screen = document.getElementById('screen');
@@ -18,6 +19,14 @@ const createBall = function() {
   ballDiv.id = 'ball_1';
   ballDiv.className = 'ball';
   screen.appendChild(ballDiv);
+};
+
+const createBrick = function() {
+  let screen = document.getElementById('screen');
+  let brickDiv = document.createElement('div');
+  brickDiv.id = 'brick_1';
+  brickDiv.className = 'brick';
+  screen.appendChild(brickDiv);
 };
 
 const addPixelSuffix = function(value) {
@@ -40,27 +49,48 @@ const drawBall = function(ball) {
   ballDiv.style.left = addPixelSuffix(ball.left);
 };
 
+const drawBrick = function(brick, id) {
+  let brickDiv = document.getElementById('brick_1');
+  let childBrickDiv = document.createElement('div');
+  childBrickDiv.id = id;
+  childBrickDiv.className = 'brick';
+  childBrickDiv.style.width = addPixelSuffix(brick.width);
+  childBrickDiv.style.height = addPixelSuffix(brick.height);
+  childBrickDiv.style.top = addPixelSuffix(brick.top);
+  childBrickDiv.style.left = addPixelSuffix(brick.left);
+  brickDiv.appendChild(childBrickDiv);
+};
+
+const removeId = function(id) {
+  let element = document.getElementById(id);
+  element.remove();
+};
+
 const handlingKeyEvent = function(paddle) {
-  if (event.key == 'ArrowRight' && !paddle.isRightBoundary())
-    paddle.moveRight();
-  if (event.key == 'ArrowLeft' && !paddle.isLeftBoundary()) paddle.moveLeft();
+  if (event.key == 'ArrowRight') paddle.moveRight();
+  if (event.key == 'ArrowLeft') paddle.moveLeft();
   drawPaddle(paddle);
 };
 
 const handlingBallMovement = function(ball, game) {
-  if (ball.isVerticalCollide()) {
+  if (ball.isTopCollide()) {
     ball.changeVerticalDirection();
   }
   if (ball.isHorizontalCollide()) {
     ball.changeHorizontalDirection();
   }
-  if (game.isCollide()) {
+  if (game.isCollidePaddleAndBall()) {
     ball.changeVerticalDirection();
   }
   if (ball.isBottomCollide()) {
     alert('game over');
     location.reload();
   }
+  if (game.isCollideBrickAndBall()) {
+    game.collideIds.forEach(x => removeId(x));
+    ball.changeVerticalDirection();
+  }
+
   ball.moveHorizontal();
   ball.moveVertical();
   drawBall(ball);
@@ -75,5 +105,9 @@ window.onload = function() {
   drawPaddle(paddle);
   createBall();
   drawBall(ball);
+  createBrick();
+  console.log(bricks);
+  bricks.createBricks();
+  Object.keys(bricks.bricks).forEach(x => drawBrick(bricks.bricks[x], x));
   moveBall(ball, game);
 };
--- model.js
+++ model.js
@@ -7,16 +7,17 @@ class Paddle {
     this.horizontalBoundary = horizontalBoundary;
   }
   moveLeft() {
-    this.left -= 10;
+    this.left -= Math.min(20, this.left);
   }
   moveRight() {
-    this.left += 10;
+    this.left += Math.min(20, this.horizontalBoundary - this.width - this.left);
   }
-  isLeftBoundary() {
-    return this.left == 0;
-  }
-  isRightBoundary() {
-    return this.left == this.horizontalBoundary - this.width;
+  isCollide(width, height, top, left) {
+    return (
+      this.top == top + height &&
+      this.left - left <= width &&
+      left - (this.left + this.width) < 0
+    );
   }
 }
 
@@ -25,16 +26,16 @@ class Ball {
     this.radius = radius;
     this.top = top;
     this.left = left;
-    this.x = -1;
-    this.y = -1;
+    this.x = 2;
+    this.y = 2;
     this.horizontalBoundary = horizontalBoundary;
     this.verticalBoundary = verticalBoundary;
   }
   moveHorizontal() {
-    this.left += this.x * 5;
+    this.left += this.x;
   }
   moveVertical() {
-    this.top += this.y * 5;
+    this.top += this.y;
   }
   changeHorizontalDirection() {
     this.x *= -1;
@@ -45,7 +46,7 @@ class Ball {
   isHorizontalCollide() {
     return this.left == 0 || this.left == this.horizontalBoundary - this.radius;
   }
-  isVerticalCollide() {
+  isTopCollide() {
     return this.top == 0;
   }
   isBottomCollide() {
@@ -53,27 +54,83 @@ class Ball {
   }
 }
 
-class bricks {
-  constructor(height, width) {
-    this.height = height;
+class Brick {
+  constructor(width, height, top, left) {
     this.width = width;
+    this.height = height;
+    this.top = top;
+    this.left = left;
   }
   invisible() {
     this.height = 0;
     this.width = 0;
   }
+  isCollide(width, height, top, left) {
+    return (
+      this.top == top - height &&
+      this.left - left < width &&
+      left - (this.left + this.width) < 0
+    );
+  }
 }
 
+class Bricks {
+  constructor(screenWidth, height, boundary) {
+    console.log(boundary);
+    this.width = Math.floor(boundary / 10);
+    this.height = height;
+    this.boundary = boundary;
+    this.bricks = {};
+    this.ids = [];
+  }
+  createBricks() {
+    let startTop = 0;
+    let startLeft = 0;
+    for (let i = 0; i < 60; i++) {
+      let brick = new Brick(this.width, this.height, startTop, startLeft);
+      this.bricks[i] = brick;
+      startLeft += this.width;
+      if (startLeft >= Math.floor(this.boundary / 10) * 10) {
+        startTop += this.height;
+        startLeft = 0;
+      }
+    }
+  }
+  remove(id) {
+    let remaining = {};
+    Object.keys(this.bricks).forEach(x => {
+      if (x != id) remaining[x] = this.bricks[x];
+    });
+    this.bricks = remaining;
+  }
+
+  getCollideBrickId(width, height, top, left) {
+    let collideIds = Object.keys(this.bricks).filter(x =>
+      this.bricks[x].isCollide(width, height, top, left)
+    );
+    collideIds.forEach(x => this.remove(x));
+    return collideIds;
+  }
+}
 class Game {
-  constructor(ball, paddle) {
+  constructor(ball, paddle, bricks) {
     this.ball = ball;
     this.paddle = paddle;
+    this.bricks = bricks;
+    this.collideIds = [];
   }
-  isCollide() {
-    return (
-      ball.top + ball.radius == paddle.top &&
-      paddle.left - ball.left < ball.radius &&
-      ball.left - (paddle.left + paddle.width) < 0
+  isCollidePaddleAndBall() {
+    let ball = this.ball;
+    return this.paddle.isCollide(ball.radius, ball.radius, ball.top, ball.left);
+  }
+  isCollideBrickAndBall() {
+    let ball = this.ball;
+    this.collideIds = this.bricks.getCollideBrickId(
+      ball.radius,
+      ball.radius,
+      ball.top,
+      ball.left
     );
+    return this.collideIds.length != 0;
   }
 }
